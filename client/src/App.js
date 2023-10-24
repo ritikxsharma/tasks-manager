60ae647338e65174ca9dc33900506cc5a0895c25
Ritik Sharma <sharmaritik2002@gmail.com>
2023-10-24 06:28:19 +0530

    initial-commit

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+        <Route path='/' element={<Login></Login>}></Route>
           <Route path='/login' element={<Login></Login>}></Route>
           <Route path='/register' element={<Register></Register>}></Route>
           <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
